89ffd576011d3dc701c11f5623da3decf14ba2df
Gapti <mark@gapti.com>
2013-05-03 13:39:31 +0100

    Added some Debug.logs

--- Assets/MarksScripts/ServerAdmin.cs
+++ Assets/MarksScripts/ServerAdmin.cs
@@ -10,6 +10,8 @@ namespace UnityProject {
 		public string serverName = "Unity Test";
 		public string gameTypeName = "MMTest123";
 		
+		public bool serverRunning = false;
+		
 		// Use this for initialization
 		void Start () {
 			
@@ -35,18 +37,37 @@ namespace UnityProject {
 		{
 			GUI.Box(new Rect(10 ,10,500	,500), "Server Menu");
 			
+			if(!serverRunning)
+			{
+			
 			if(GUI.Button(new Rect(200,100,150,50),"Connect to Server"))
 			{
 				MasterServer.ClearHostList();
 				MasterServer.RequestHostList(gameTypeName);
 			}
 			
-			if(GUI.Button(new Rect(200,300,150,50),"Start Server"))
-			{
-				Network.InitializeServer(10,25002, !Network.HavePublicAddress()); //// if we need NAT it will use it.
-				MasterServer.RegisterHost (gameTypeName, serverName, "Test Unity Network");
-			}
 			
+				if(GUI.Button(new Rect(200,300,150,50),"Start Server"))
+				{
+					Network.InitializeServer(10,25002, !Network.HavePublicAddress()); //// if we need NAT it will use it.
+					MasterServer.RegisterHost (gameTypeName, serverName, "Test Unity Network");
+				}
+			}
+			else
+				GUI.Label(new Rect(200,100,200,50),"Server started and running");
+				
+			
+		}
+		
+		void OnServerInitialized()
+		{
+			Debug.Log ("Server started");	
+			serverRunning = true;
+		}
+		
+		void OnConnectedToServer()
+		{
+			Debug.Log ("You are connected to a server");	
 		}
 		
 	}
