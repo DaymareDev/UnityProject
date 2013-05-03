7843315fcc3c9d30e4feee779f9080163d99135b
Gapti <mark@gapti.com>
2013-05-03 12:35:42 +0100

    Added nameSpace to ServerAdmin

--- Assets/MarksScripts/ServerAdmin.cs
+++ Assets/MarksScripts/ServerAdmin.cs
@@ -1,33 +1,36 @@
 using UnityEngine;
 using System.Collections;
 
-public class ServerAdmin : MonoBehaviour {
-	
-	public string ip;
-	public int port;
-	public string serverName = "Unity Test";
-	public string gameTypeName = "MMTest123";
-	
-	// Use this for initialization
-	void Start () {
+namespace UnityProject {
+
+	public class ServerAdmin : MonoBehaviour {
 		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-	
-	void OnGUI()
-	{
-		GUI.Box(new Rect(10 ,10,500	,500), "Server Menu");
-		GUI.Button(new Rect(200,100,150,50),"Connect to Server");
+		public string ip;
+		public int port;
+		public string serverName = "Unity Test";
+		public string gameTypeName = "MMTest123";
 		
-		if(GUI.Button(new Rect(200,300,150,50),"Start Server"))
-		{
-			Network.InitializeServer(10,25002, !Network.HavePublicAddress()); //// if we need NAT it will use it.
-			MasterServer.RegisterHost (gameTypeName, serverName, "Test Unity Network");
+		// Use this for initialization
+		void Start () {
+			
 		}
 		
+		// Update is called once per frame
+		void Update () {
+		
+		}
+		
+		void OnGUI()
+		{
+			GUI.Box(new Rect(10 ,10,500	,500), "Server Menu");
+			GUI.Button(new Rect(200,100,150,50),"Connect to Server");
+			
+			if(GUI.Button(new Rect(200,300,150,50),"Start Server"))
+			{
+				Network.InitializeServer(10,25002, !Network.HavePublicAddress()); //// if we need NAT it will use it.
+				MasterServer.RegisterHost (gameTypeName, serverName, "Test Unity Network");
+			}
+			
+		}
 	}
 }
