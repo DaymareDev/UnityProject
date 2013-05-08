a202a3647a04b6e739fbff3bb57a71bf8c998427
Gapti <mark@gapti.com>
2013-05-08 12:33:27 +0100

    Added pick up and throw ball

--- Assets/MarksScripts/ThrowBall.cs
+++ Assets/MarksScripts/ThrowBall.cs
@@ -6,11 +6,17 @@ namespace UnityProject{
 	public class ThrowBall : MonoBehaviour {
 	
 		private bool _holdingBall = false;
+		public GameObject ball;
 		
-		 void OnCollisionEnter(Collision obj) 
+		void OnAwake()
 		{
-			print (obj.gameObject.name);
-			if(obj.gameObject.name == "Ball")
+			ball = null;	
+		}
+		
+		 void OnControllerColliderHit(ControllerColliderHit obj)  
+		{
+		
+			if(obj.gameObject.name == "Ball" && !_holdingBall)
 			{
 				HoldBall( obj.gameObject );	
 			}
@@ -18,7 +24,30 @@ namespace UnityProject{
 		
 		void HoldBall(GameObject obj)
 		{
+			obj.rigidbody.useGravity = false;
 			obj.transform.parent = this.transform;
+			obj.transform.localPosition = new Vector3(1,1,1);
+			
+			_holdingBall = true;
+			
+			ball = obj;
+		}
+		
+		void Update()
+		{
+			
+			Vector3 throwDirection = this.transform.forward;
+			
+			if(_holdingBall)
+				if(Input.GetMouseButtonDown(0))
+				{
+					ball.transform.parent = null;
+					ball.rigidbody.useGravity = true;
+					ball.rigidbody.AddForce(throwDirection * 1000);
+				
+				ball = null;
+				_holdingBall = false;
+				}
 		}
 	}
 }
